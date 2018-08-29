3207b64100db1a706dad3667bd8e849191b9db9f
Daniel Gonzalez <dgonzalez93@gmail.com>
2018-08-29 10:34:30 -0500

    ADDED: removeBack functionality

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -107,9 +107,22 @@ bool LinkedList<T>::removeBack()
 	Node<T>* secondintoLast = nullptr;
 	bool isRemoved = false;
 
-	/** TODO 
-		Fix this method
-	*/
+	if(!isEmpty())
+	{
+		lastNode = m_front;
+		while(lastNode->getNext() != nullptr)
+		{
+			secondintoLast = lastNode;
+			lastNode = lastNode->getNext();
+		}
+		if (secondintoLast != nullptr)
+		{
+			secondintoLast->setNext(nullptr);
+		}
+		delete lastNode;
+		m_size--;
+		isRemoved = true;
+	}
 
 	return(isRemoved);
 }	
